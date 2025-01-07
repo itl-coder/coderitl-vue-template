0e2ce3b0111b7e95162b651db67ad5f2448a4993
user-system <ituninapp@gmail.com>
2025-01-07 14:42:17 +0800

    feat: 基础环境搭建

---
+++ coderitl-common/src/test/java/com/coderitl/coderitl/common/CoderitlCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderitl.coderitl.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderitlCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ coderitl-flowable/src/test/java/com/coderitl/coderitl/flowable/CoderitlFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderitl.coderitl.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderitlFlowableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
